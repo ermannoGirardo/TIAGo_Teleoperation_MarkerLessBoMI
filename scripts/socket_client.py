b97618bd9d736ea87304b9494e22819c974bb554
ermannoGirardo <48509825+ermannoGirardo@users.noreply.github.com>
2022-05-17 17:52:46 +0200

    Modifications

--- scripts/socket_client.py
+++ scripts/socket_client.py
@@ -76,7 +76,7 @@ def client_connected(connection):
     Function to wait time_ms
     """
     global send,bytes_to_send,close_connection
-    HOST = "192.168.56.1"  # The server's hostname or IP address
+    HOST = "192.168.1.22"  # The server's hostname or IP address
     PORT = 5050  # The port used by the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
@@ -88,6 +88,6 @@ def client_connected(connection):
             if close_connection:
                 #send disconnect message
                 s.sendall(DISCONNECT_BYTES_MESSAGE)
-                break   #Mandare messaggio disconnessione per terminare serve ok + aggiungere esecuzione file 
+                break    
             time.sleep(0.001)
             
